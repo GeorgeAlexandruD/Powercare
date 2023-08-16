034baf6ff6f8473c9384eeb8179213f0b61f5873
GeorgeAlexandruD <donj@desktop>
2023-08-16 09:52:41 +0200

    Api with logging app

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Power.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public  ActionResult<string> Get()
+        {
+             _logger.LogInformation("All users have been returned" );
+            return "Users [...]";
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post()
+        {
+            _logger.LogInformation("User has been updated");
+            return "User has been updated";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Host.UseSerilog((hostContext, services, configuration) => 
+            {
+                configuration
+                .MinimumLevel.Information()
+                .WriteTo.File("C:\\log\\test\\AppLogss.log")
+                .WriteTo.Console();
+            });
+
+            var app = builder.Build();
+
+            app.UseRouting();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
